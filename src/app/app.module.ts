a2bed00d67e5b11648cea329135ace030641d7f2
Ron <Rayron5656@Gmail.com>
2022-05-19 17:08:56 +0300

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './Components/movie-list/movie-list.component';
+import { WatchListComponent } from './Components/watch-list/watch-list.component';
+
+const appRoutes: Routes = [
+  { path: 'movielist', component: MovieListComponent },
+  { path: 'watchlist', component: WatchListComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    WatchListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieListComponent,
+    WatchListComponent
   ],
   imports: [
     BrowserModule,
