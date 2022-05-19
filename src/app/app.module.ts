a3acdefc60a0bc030ee2fb926966f595d46d9de8
Ron <Rayron5656@Gmail.com>
2022-05-19 17:15:04 +0300

    add module and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgbModal
     
   ],
   providers: [],
@@ -30,17 +32,4 @@ const appRoutes: Routes = [
 export class AppModule { }
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    MovieListComponent,
-    WatchListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+
