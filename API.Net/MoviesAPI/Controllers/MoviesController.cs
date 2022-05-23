411226416c89b5fc51dd91799900040bbabc4f2a
Ron <Rayron5656@Gmail.com>
2022-05-23 21:34:12 +0300

    MovieApi.Net

--- API.Net/MoviesAPI/Controllers/MoviesController.cs
+++ API.Net/MoviesAPI/Controllers/MoviesController.cs
@@ -20,7 +20,7 @@ namespace MoviesAPI.Controllers
             _context = context;
         }
 
-        // GET: Movies
+        
         [EnableCors]
         public async Task<IActionResult> Index()
         {
@@ -32,11 +32,11 @@ namespace MoviesAPI.Controllers
         [Route("[Controller]/GetMovies")]
         public async Task<IActionResult> GetMovies()
         {
-            //return View(await _context.Movie.ToListAsync());
+            
             return Ok(await _context.Movie.ToListAsync());
         }
 
-        // GET: Movies/Details/5
+        
         [Route("[Controller]/{id}")]
         public async Task<IActionResult> Details(int? id)
         {
@@ -55,15 +55,13 @@ namespace MoviesAPI.Controllers
             return Ok(movie);
         }
 
-        // GET: Movies/Create
+        
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Movies/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Summery,Genre,Image,Score")] Movie movie)
@@ -77,7 +75,7 @@ namespace MoviesAPI.Controllers
             return View(movie);
         }
 
-        // GET: Movies/Edit/5
+        
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -93,9 +91,7 @@ namespace MoviesAPI.Controllers
             return View(movie);
         }
 
-        // POST: Movies/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Summery,Genre,Image,Score")] Movie movie)
@@ -128,7 +124,7 @@ namespace MoviesAPI.Controllers
             return View(movie);
         }
 
-        // GET: Movies/Delete/5
+        
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -146,7 +142,7 @@ namespace MoviesAPI.Controllers
             return View(movie);
         }
 
-        // POST: Movies/Delete/5
+        
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
